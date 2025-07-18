50ce886aa5a675c367ae4a84095ee2a8b50fc717
atharvabhuse <atharvabhuse1@gmailcom>
2025-07-19 00:07:59 +0530

    light mode initial

--- src/Context.js
+++ src/Context.js
@@ -1,29 +1,27 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useReducer } from "react";
 
-export const themeContext=createContext()
+export const themeContext = createContext();
 
-const initialState={darkMode: true}
+const initialState = { darkMode: false };
 
-const themeReducer=(state,action)=>{
-    switch(action.type){
-        
-        case 'toggle':
-            return{
-                darkMode: !state.darkMode
-            }
-        default:
-            return{
-                darkMode: false
-            }
-    }
-}
+const themeReducer = (state, action) => {
+  switch (action.type) {
+    case "toggle":
+      return {
+        darkMode: !state.darkMode,
+      };
+    default:
+      return {
+        darkMode: false,
+      };
+  }
+};
 
-export const Themeprovider=(props)=>{
-
-    const [state,dispatch]=useReducer(themeReducer, initialState)
-    return(
-        <themeContext.Provider value={{state,dispatch}}>
-            {props.children}
-        </themeContext.Provider>
-    )
-}
+export const Themeprovider = (props) => {
+  const [state, dispatch] = useReducer(themeReducer, initialState);
+  return (
+    <themeContext.Provider value={{ state, dispatch }}>
+      {props.children}
+    </themeContext.Provider>
+  );
+};
